f7956cfb7e50cfb77be2af39d7a5462d92d47ee4
CodebyChris07 <chrisjalu07@gmail.com>
2026-01-21 12:50:35 -0500

    ejercicio en el paquete02 resuelto

--- EjemplosExcepciones/src/Paquete02/ejercicio.java
+++ EjemplosExcepciones/src/Paquete02/ejercicio.java
@@ -4,10 +4,50 @@
  */
 package Paquete02;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ASUS
  */
 public class ejercicio {
-    
+
+    static Scanner entrada = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int valor1 = ingresarValor();
+        int valor2 = ingresarValor();
+        int result = obtenerSuma(valor1, valor2);
+        imprimir(valor1, valor2, result);
+
+    }
+
+    public static int ingresarValor() {
+        int valor = 0;
+        try {
+            boolean bandera = true;
+            while (bandera) {
+                System.out.println("Ingrese el valor:");
+                valor = entrada.nextInt(); // se espera un valor entero 
+                if (valor < 0 || valor % 2 == 1) {
+                    throw new Exception("valor invalido");
+                }
+                bandera = false;
+            }
+        } catch (Exception e) {
+            System.out.printf(" Lo sentimos hay un error de tipo %s\n", e);
+        }
+        return valor;
+    }
+
+    public static void imprimir(int a, int b, int c) {
+System.out.printf("Los valores que se ingresaron son %d y %s\nResultado de la "
+        + "suma: %d\n", a, b, c);
+    }
+
+    public static int obtenerSuma(int a, int b) {
+        int resultado;
+        resultado = a + b;
+        return resultado;
+    }
 }
